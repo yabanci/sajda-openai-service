c0fc86f6f630021716acd7ebcef82cd8ffac304f
yabanci <ozhetov.arsen@gmail.com>
2024-07-27 15:10:01 +0300

    feat: api

---
+++ api/routes.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, HTTPException
+
+from core.trip_planner import TripPlanner
+from models.trip_request import TripRequest
+
+router = APIRouter()
+
+
+@router.post("/trip")
+async def plan_trip(request: TripRequest):
+    try:
+        trip_planner = TripPlanner()
+        response = await trip_planner.plan_trip(request)
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
